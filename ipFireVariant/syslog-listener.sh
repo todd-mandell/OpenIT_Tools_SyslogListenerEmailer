ef206e9775ab327285144870678977d0f307e9a5
Todd Mandell <todd.mandell@outlook.com>
2026-03-11 22:13:11 -0400

    Update syslog-listener.sh
    BUSYBOX FRIENDLY

--- ipFireVariant/syslog-listener.sh
+++ ipFireVariant/syslog-listener.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 LOGFILE="/home/openitmailer/syslog-email-service.log"
+SMTP="/home/openitmailer/smtp-send.sh"
 
 PORT1=514
 PORT2=162
@@ -8,22 +9,21 @@ PORT2=162
 echo "$(date) Starting syslog listeners on UDP $PORT1 and $PORT2" >> "$LOGFILE"
 
 handle_line() {
-    local LINE="$1"
-    local TS
+    LINE="$1"
     TS=$(date +"%Y-%m-%d %H:%M:%S")
 
     echo "$TS Received: $LINE" >> "$LOGFILE"
 
-    /home/openitmailer/smtp-send.sh "Syslog Alert: $TS" "$LINE"
+    $SMTP "Syslog Alert: $TS" "$LINE"
 }
 
 # Listener for port 514
-nc -klu $PORT1 | while read -r LINE; do
+nc -klu $PORT1 | while read LINE; do
     handle_line "$LINE"
 done &
 
 # Listener for port 162
-nc -klu $PORT2 | while read -r LINE; do
+nc -klu $PORT2 | while read LINE; do
     handle_line "$LINE"
 done &
 
